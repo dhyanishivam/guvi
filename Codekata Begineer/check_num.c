3ef8a088658372a80488899228eedfe09b81d864
sushilsingh23 <35341499+sushilsingh23@users.noreply.github.com>
2018-01-11 22:47:42 +0530

    Update check_num.c

--- Codekata Begineer/check_num.c
+++ Codekata Begineer/check_num.c
@@ -4,7 +4,7 @@ int main()
     float n;
     printf("Enter a number: \n");
     scanf("%f",&n);
-    (n<0)?printf("negative numbe\n"):
-    (n>0)?printf("%.2f is a positive number\n",n):printf("Input is Zero\n");
+    (n<0)?printf("Negative\n"):
+    (n>0)?printf("Positive\n",n):printf("Zero\n");
     return 0;
 }
